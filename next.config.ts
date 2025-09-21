36627692b0643c48daf94d5a17dee04f30da6db4
skyboy <notnotsky26@gmail.com>
2025-09-21 09:11:03 +0530

    Fix Next.js config by removing deprecated experimental options

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  experimental: {
-    serverComponentsExternalPackages: ['embla-carousel-react'],
-  },
   /* config options here */
 };
 
